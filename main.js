934de7ed4c44b8ec2ba50b12ee8c7db750b5b8eb
Hiras <mhmdhiras@gmail.com>
2018-01-06 00:34:17 +0530

    console loged the api call

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Local Weather App'
+        title: 'Local Weather App',
+        weatherData: []
+    },
+
+    created() {
+        this.getWeatherData()
+    },
+
+    methods: {
+        getWeatherData: function () {
+            axios.get("https://api.openweathermap.org/data/2.5/weather?appid=7dd3552e9a532470997bbefc96969b50&q=London")
+            .then((resp) => {
+                this.weatherData = resp.data
+                console.log(resp.data)
+            })
+            .catch((e) => {
+                console.error(e)
+            })
+        }
     }
 })
